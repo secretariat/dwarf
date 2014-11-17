6ced8a97f8ad5a5b067f467731cf3be4c18006f3
«Secretariat1987» <mixey.viti@bigmir.net>
2014-11-17 16:05:27 +0200

    start dwarfing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dwarf::Application.config.secret_token = 'ae085f48f6d08cbd6a882e67f2549addabf94cd95821b6359a898ef9098490d34c92033b1d7a85a6a891e3a11abf1164d3c86da09a2fd97f10667490572ec93d'
